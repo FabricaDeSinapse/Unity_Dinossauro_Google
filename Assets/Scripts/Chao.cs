276ffc964539a27d9abcadabd42dc7603c49d7d9
Paulo Salvatore <paulo@salvatore.tech>
2020-07-09 03:43:33 -0300

    Chão infinito implementado

---
+++ Assets/Scripts/Chao.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chao : MonoBehaviour
+{
+    public float diferencaX;
+
+    public float minX;
+
+    private void Update()
+    {
+        if (transform.position.x <= minX)
+        {
+            transform.position = new Vector3(
+                transform.position.x + diferencaX * 2,
+                transform.position.y,
+                transform.position.z
+            );
+        }
+    }
+}
